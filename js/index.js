8116a82ec156bb0e601f3a266458bfaa629b0178
Atsighi Bright <brytebee@gmail.com>
2021-10-18 14:42:45 +0100

    Add book function

--- js/index.js
+++ js/index.js
@@ -5,37 +5,44 @@ let books = [
     author: 'Blessed Jason Mwanza',
   },
 ];
-const displayBooks = () =>{
+const displayBooks = () => {
   booksElement.innerHTML = '';
   for (const bookId in books) {
     if (Object.hasOwnProperty.call(books, bookId)) {
       const book = books[bookId];
       booksElement.innerHTML += `
-        <span class="book" >
-          <span>${book.title}</span>
-          <br>
-          <span>${book.author}</span>
-          <br>
-          <button onclick="removeBook(${bookId});">Remove</button>
-          <hr>
-        </span>
-      `;
+          <span class="book" >
+            <span>${book.title}</span>
+            <br>
+            <span>${book.author}</span>
+            <br>
+            <button onclick="removeBook(${bookId});">Remove</button>
+            <hr>
+          </span>
+        `;
     }
   }
-}
-const addBook = (title, author) =>{
+};
+const addBook = (title, author) => {
   books.push({
     title: title,
     author: author,
   });
   displayBooks();
-}
-const removeBook = (bookId) =>{
-  if(bookId !== null && bookId !== undefined){
+};
+const removeBook = (bookId) => {
+  if (bookId !== null && bookId !== undefined) {
     books.splice(bookId, 1);
     displayBooks();
   }
-}
+};
 displayBooks();
-addBook('Title here', 'blessed');
-removeBook(1);
+
+const submitForm = document.querySelector('.add-books-form');
+submitForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let bookAuthor = document.querySelector('#book-author').value;
+  let bookTitle = document.querySelector('#book-title').value;
+  addBook(bookAuthor, bookTitle);
+  submitForm.reset();
+});
