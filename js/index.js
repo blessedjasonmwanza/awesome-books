b5b2f87494e29ca1d41583c0be6d0868b53b3f38
blessedjasonmwanza <mwanzabj@gmail.com>
2021-10-19 11:36:51 +0200

    Wrapp traditional JS functions as methods in a class

--- js/index.js
+++ js/index.js
@@ -1,49 +1,41 @@
-const booksElement = document.querySelector('#books-list');
 const savedBooks = localStorage.getItem('books');
-let books = [];
-if (savedBooks && savedBooks !== null) {
-  books = JSON.parse(savedBooks);
+class Books {
+  constructor(savedBooks) {
+    this.savedBooks = savedBooks;
+    this.booksElement = document.querySelector('#books-list');
+  }
+
+  display() {
+    this.booksElement.innerHTML = '';
+    for (let bookId = 0; bookId < this.savedBooks.length; bookId += 1) {
+      const book = this.savedBooks[bookId];
+      this.booksElement.innerHTML += `
+          <span class="book" >
+            <span>${book.title}</span>
+            <br>
+            <span>${book.author}</span>
+            <br>
+            <button onclick="removeBook(${bookId});">Remove</button>
+            <hr>
+          </span>
+        `;
+    }
+  }
+
+  add(bookTitle, bookAuthor) {
+    this.savedBooks.push({
+      title: bookTitle,
+      author: bookAuthor,
+    });
+    localStorage.setItem('books', JSON.stringify(this.savedBooks));
+    this.display();
+  }
+
+  remove(bookId) {
+    if (bookId !== null && bookId !== undefined) {
+      this.savedBooks.splice(bookId, 1);
+      localStorage.setItem('books', JSON.stringify(this.savedBooks));
+      this.display();
+    }
+  }
 }
-
-const displayBooks = () => {
-  booksElement.innerHTML = '';
-  for (let bookId = 0; bookId < books.length; bookId += 1) {
-    const book = books[bookId];
-    booksElement.innerHTML += `
-        <span class="book" >
-          <span>${book.title}</span>
-          <br>
-          <span>${book.author}</span>
-          <br>
-          <button onclick="removeBook(${bookId});">Remove</button>
-          <hr>
-        </span>
-      `;
-  }
-};
-
-const addBook = (bookTitle, bookAuthor) => {
-  books.push({
-    title: bookTitle,
-    author: bookAuthor,
-  });
-  localStorage.setItem('books', JSON.stringify(books));
-  displayBooks();
-};
-const removeBook = (bookId) => {
-  if (bookId !== null && bookId !== undefined) {
-    books.splice(bookId, 1);
-    localStorage.setItem('books', JSON.stringify(books));
-    displayBooks();
-  }
-};
-displayBooks();
-removeBook();
-const submitForm = document.querySelector('.add-books-form');
-submitForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const bookAuthor = document.querySelector('#book-author').value;
-  const bookTitle = document.querySelector('#book-title').value;
-  addBook(bookAuthor, bookTitle);
-  submitForm.reset();
-});
