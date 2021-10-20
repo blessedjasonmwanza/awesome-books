e8044674bf431546035901a538ed4d941ef72db9
Atsighi Bright <brytebee@gmail.com>
2021-10-20 11:14:05 +0100

    Add complete website navigation and functionalities

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 class Books {
   constructor(savedBooks) {
     this.savedBooks = savedBooks;
-    this.booksElement = document.querySelector('#books-list');
+    this.booksElement = document.querySelector("#books-list");
   }
 
   display() {
-    this.booksElement.innerHTML = '';
+    this.booksElement.innerHTML = "";
     for (let bookId = 0; bookId < this.savedBooks.length; bookId += 1) {
       const book = this.savedBooks[bookId];
       this.booksElement.innerHTML += `
@@ -26,19 +26,19 @@ class Books {
       title: bookTitle,
       author: bookAuthor,
     });
-    localStorage.setItem('books', JSON.stringify(this.savedBooks));
+    localStorage.setItem("books", JSON.stringify(this.savedBooks));
     this.display();
   }
 
   remove(bookId) {
     if (bookId !== null && bookId !== undefined) {
       this.savedBooks.splice(bookId, 1);
-      localStorage.setItem('books', JSON.stringify(this.savedBooks));
+      localStorage.setItem("books", JSON.stringify(this.savedBooks));
       this.display();
     }
   }
 }
-let savedBooks = localStorage.getItem('books');
+let savedBooks = localStorage.getItem("books");
 if (savedBooks) {
   savedBooks = JSON.parse(savedBooks);
 } else {
@@ -49,12 +49,31 @@ books.display();
 const removeBook = (bookId) => books.remove(bookId);
 removeBook();
 
-const submitForm = document.querySelector('.add-books-form');
+const submitForm = document.querySelector(".add-books-form");
 
-submitForm.addEventListener('submit', (e) => {
+submitForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const bookAuthor = document.querySelector('#book-author').value;
-  const bookTitle = document.querySelector('#book-title').value;
+  const bookAuthor = document.querySelector("#book-author").value;
+  const bookTitle = document.querySelector("#book-title").value;
   books.add(bookAuthor, bookTitle);
   submitForm.reset();
+});
+
+const booksLink = document.querySelector("#navBook");
+const addLink = document.querySelector("#navAdd");
+const contactLink = document.querySelector("#navContact");
+const links = [booksLink, addLink, contactLink];
+
+const booksPage = document.querySelector("#books");
+const addPage = document.querySelector("#add");
+const contactPage = document.querySelector("#contact");
+const pages = [booksPage, addPage, contactPage];
+
+links.forEach((link, i) => {
+  link.addEventListener("click", function () {
+    const target = pages.filter((id) => i === pages.indexOf(id));
+    const nonTarget = pages.filter((id) => i !== pages.indexOf(id));
+    console.log(target[0].classList.remove("hidden"));
+    for (const otherPages of nonTarget) otherPages.classList.add("hidden");
+  });
 });
